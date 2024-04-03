b30c74d0b78634a62c8e15fe2fd0511d64e0ae02
Priya Thevar <priya.thevar89@gmail.com>
2024-04-04 01:20:02 +0400

    Added all pages and gitignore

---
+++ App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <header>
+      <Link to="/">#VANLIFE</Link>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+    </Routes>
+  </BrowserRouter>
+
+     
+    
+  );
+}
+
+export default App;
